c7ea634fcfbc67740b6de46e26580732276f0e1a
yaattzy <yeratzyen@gmail.com>
2021-09-17 14:54:03 -0600

    Final TTS

--- TTS/app/src/main/java/com/ye/tts/MainActivity.kt
+++ TTS/app/src/main/java/com/ye/tts/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
@@ -16,23 +18,39 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         setContentView(R.layout.activity_main)
 
         textToSpeech = TextToSpeech(this, this)
-        
-        findViewById<Button>(R.id.btn_play).setOnClickListener{ hablar()}
+
+        findViewById<Button>(R.id.btn_play).setOnClickListener {
+           hablar()
+        }
 
     }
 
     fun hablar() {
-        var mensaje : String = findViewById<TextView>(R.id.txt_hello).text.toString()
+        var mensaje : String = findViewById<EditText>(R.id.ed_mensaje).text.toString()
+
+        if(mensaje.isEmpty()) {
+            Toast.makeText(this, "Por favor, ingrese un mensaje", Toast.LENGTH_SHORT).show()
+        }
+
         textToSpeech?.speak(mensaje, TextToSpeech.QUEUE_FLUSH, null, "")
     }
+
 
     override fun onInit(status: Int) {
         var mensaje : TextView = findViewById<TextView>(R.id.txt_hello)
         if(status == TextToSpeech.SUCCESS) {
             mensaje.text = "Hola, si se pudo c:"
-            textToSpeech?.setLanguage(Locale.GERMANY)
+            textToSpeech?.language = Locale("ES")
         } else {
-            mensaje.text = "No se puso :c"
+            mensaje.text = "No se pudo :c"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(textToSpeech != null) {
+            textToSpeech?.stop()
+            textToSpeech?.shutdown()
         }
     }
 }
