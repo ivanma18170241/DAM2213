ab6eaa402ef5c2dc261fd7b57e86cb1256605173
yaattzy <yeratzyen@gmail.com>
2021-09-20 15:02:52 -0600

    Linear y Relative Layout

---
+++ Layout/app/src/main/java/com/ye/layout/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.ye.layout.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Linear Layout"
+    }
+    val text: LiveData<String> = _text
+}
