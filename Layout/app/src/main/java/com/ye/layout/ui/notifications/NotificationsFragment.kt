8a82d220c46cd08cf73cd3e715a93f12b1117f29
yaattzy <yeratzyen@gmail.com>
2021-09-21 14:56:38 -0600

    Inicio constraint

--- Layout/app/src/main/java/com/ye/layout/ui/notifications/NotificationsFragment.kt
+++ Layout/app/src/main/java/com/ye/layout/ui/notifications/NotificationsFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ye.layout.R
-import com.ye.layout.databinding.FragmentNotificationsBinding
+import com.ye.layout.databinding.FragmentConstraintBinding
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentConstraintBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +28,7 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentConstraintBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
