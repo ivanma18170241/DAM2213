5760c05f69e5c9d44cf6a5e2be64e4173da24e91
yaattzy <yeratzyen@gmail.com>
2021-10-01 15:04:32 -0600

    Cambios dinamicos en elementos

--- Binding/app/src/main/java/com/dam/ye/databinding/MainActivity.kt
+++ Binding/app/src/main/java/com/dam/ye/databinding/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.dam.ye.databinding
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.dam.ye.databinding.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +22,43 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnAdd.setOnClickListener {
             Toast.makeText(this, "Vinculación", Toast.LENGTH_SHORT).show()
+            addAction()
         }
+
+        binding.btnSend.setOnClickListener {
+            binding.cvPerson.visibility = View.GONE
+        }
+
+        binding.btnSkip.setOnClickListener { view ->
+            binding.cvPerson.visibility = View.GONE
+            Snackbar.make(view, "Ocultando persona", Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.btnAdd)
+                .setAction("Deshacer") {
+                    binding.cvPerson.visibility = View.VISIBLE
+                    Toast.makeText(this, "Deshacer acción activada", Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+        binding.enable.setOnClickListener {
+            binding.tilLink.isEnabled = binding.enable.isChecked
+        }
+    }
+
+    private fun addAction() {
+       val idSelection = binding.rgColors.checkedRadioButtonId
+
+        when(idSelection) {
+            R.id.rbRed -> binding.imgOther.setBackgroundColor(Color.parseColor("#FF0000"))
+            R.id.rbBlue -> binding.imgOther.setBackgroundColor(Color.parseColor("#0000FF"))
+            else -> binding.imgOther.setBackgroundColor(Color.parseColor("#CCCCCC"))
+        }
+
+
+        Glide.with(this) // Contexto
+            .load(binding.edLink.text.toString()) // URL
+            .centerCrop()
+            .into(binding.imgOther)
+
     }
 }
